5d5dd84191bd7025f611aa906a4611b6dc0de04a
a.a.babichev <a.a.babichev@84095216-3abd-11de-90e1-f9f1724fc9cc>
2009-05-16 13:05:37 +0000

    --help and --version switches added to slava
    git-svn-id: http://sasl.googlecode.com/svn/trunk@22 84095216-3abd-11de-90e1-f9f1724fc9cc

--- slava/cmdline.cpp
+++ slava/cmdline.cpp
@@ -1,11 +1,50 @@
 #include "cmdline.h"
 
-#include "utils.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "utils.h"
+#include "../version.h"
 
 
 using namespace slava;
+
+
+/// Print SLAVA version and exit
+static void printVersion()
+{
+#ifdef SNAPSHOT
+#define QUOTE(x) #x
+    printf("Stand-Alone Avionics Application snapshot %i.%i.%i %s\n", 
+            VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, QUOTE(SNAPSHOT));
+#undef QUOTE
+#else
+    printf("Stand-Alone Avionics Application v%i.%i.%i\n", 
+            VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+#endif
+    exit(0);
+}
+
+
+/// Print short command line help and exit
+static void printHelp()
+{
+    printf("USAGE:\n");
+    printf("  slava [options]\n");
+    printf("OPTIONS:\n");
+    printf("  --host <hostname>    - address of flight simulator server\n");
+    printf("  --port <portnumber>  - port number at flight simulator server\n");
+    printf("  --secret <password>  - flight simulator password\n");
+    printf("  --width <pixels>     - width of window\n");
+    printf("  --height <pixels>    - height of window\n");
+    printf("  --fullscreen         - enable fullscreen mode\n");
+    printf("  --panel <file>       - path to panel lua file\n");
+    printf("  --data <path>        - location of sasl data dir\n");
+    printf("  --fps <limit>        - limit maximum FPS (use 0 for unlimited)\n");
+    printf("  --version            - print version number\n");
+    printf("  --help               - print this help\n");
+    exit(0);
+}
 
 
 slava::CmdLine::CmdLine(int argc, char *argv[]): 
@@ -36,6 +75,10 @@ slava::CmdLine::CmdLine(int argc, char *argv[]):
             dataDir = std::string(argv[++i]);
         else if ((! strcmp(argv[i], "--fps")) && (i < argc - 1))
             targetFps = strToInt(argv[++i]);
+        else if (! strcmp(argv[i], "--version"))
+            printVersion();
+        else if (! strcmp(argv[i], "--help"))
+            printHelp();
         else {
             printf("Invalid option '%s'.\n", argv[i]);
             exit(1);
