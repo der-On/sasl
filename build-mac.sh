2b5f1a1d9f9af3e676c707850b7cae0fbf3b0189
me@julik.nl <me@julik.nl@84095216-3abd-11de-90e1-f9f1724fc9cc>
2011-08-01 14:55:32 +0000

    Latest changes received from Felis by middle of an ass-backwards Narod link
    git-svn-id: http://sasl.googlecode.com/svn/trunk@155 84095216-3abd-11de-90e1-f9f1724fc9cc

--- build-mac.sh
+++ build-mac.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 # Shotgun builder for all of the OS X components
-cd libavionics; make clean; make -f Makefile.mac
-cd ../slava; make clean; make -f Makefile.mac
-cd ../xap; make clean; make -f Makefile.mac
-cp mac.xpl "/Users/julik/Desktop/X-Plane-Beta/Aircraft/IL-14_930/plugins/xap/mac.xpl"
+cd libogl; make clean -f Makefile.mac; make -f Makefile.mac
+cd libavionics; make clean -f Makefile.mac; make -f Makefile.mac
+# skip slava compilation
+# cd ../slava; make clean; make -f Makefile.mac
+cd ../xap; make clean -f Makefile.mac; make -f Makefile.mac
+# cp mac.xpl "/Users/julik/Desktop/X-Plane-Beta/Aircraft/IL-14_930/plugins/xap/mac.xpl"
--- libogl/ogl.cpp
+++ libogl/ogl.cpp
@@ -1,6 +1,5 @@
 #include "ogl.h"
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
@@ -11,50 +10,14 @@
 // graphics context
 struct OglCanvas
 {
-    /// pointers to this callbacks
     struct XaGraphicsCallbacks callbacks;
-
-    /// texture binder function (or NULL if not available)
     xagl_bind_texture_2d_callback binderCallback;
-
-    /// texture ID generator (or NULL if not available)
     xagl_gen_tex_name_callback genTexNameCallback;
 
-    // number of triangles drawn
     int triangles;
-
-    /// number of lines drawn
     int lines;
-
-    /// number of textures loaded
     int textures;
-
-    /// size of loaded textures
     int texturesSize;
-
-    // number of batches drawn
-    int batches;
-
-    /// current texture ID or 0 if texturing disabled
-    int currentTexture;
-
-    /// what to draw: GL_LINES or GL_TRIANGLES
-    int currentMode;
-
-    /// maximum size of vertex buffer
-    int maxVertices;
-
-    /// current number of vertices in buffer
-    int numVertices;
-
-    /// vertices buffer
-    float *vertexBuffer;
-    
-    /// texture coords buffer
-    float *texBuffer;
-    
-    /// vertex colors buffer
-    float *colorBuffer;
 };
 
 
@@ -65,134 +28,22 @@ static void drawBegin(struct XaGraphicsCallbacks *canvas)
     OglCanvas *c = (OglCanvas*)canvas;
     assert(canvas);
 
-    glPushAttrib(GL_ALL_ATTRIB_BITS);
-    glPushClientAttrib(GL_CLIENT_ALL_ATTRIB_BITS);
-
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_TEXTURE_2D);
-    c->currentTexture = -1;
-
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    glVertexPointer(2, GL_FLOAT, 0, c->vertexBuffer);
-    glTexCoordPointer(2, GL_FLOAT, 0, c->texBuffer);
-    glColorPointer(4, GL_FLOAT, 0, c->colorBuffer);
 
     c->triangles = 0;
     c->lines = 0;
-    c->batches = 0;
-    
-    c->numVertices = 0;
-    c->currentTexture = 0;
-    c->currentMode = GL_TRIANGLES;
 }
-
-
-/// make sure vertex buffer is large enough to fit qty vertices
-/// \param qty - additional vertices to add to vertex buffer
-static void reserveSpace(OglCanvas *c, int qty)
-{
-    if (c->numVertices + qty > c->maxVertices) {
-        c->maxVertices += (qty / 1024 + 1) * 1024;
-        int s = sizeof(float) * c->maxVertices;
-        c->vertexBuffer = (float*)realloc(c->vertexBuffer, 2 * s);
-        c->texBuffer = (float*)realloc(c->texBuffer, 2 * s);
-        c->colorBuffer = (float*)realloc(c->colorBuffer, 4 * s);
-        
-        glVertexPointer(2, GL_FLOAT, 0, c->vertexBuffer);
-        glTexCoordPointer(2, GL_FLOAT, 0, c->texBuffer);
-        glColorPointer(4, GL_FLOAT, 0, c->colorBuffer);
-    }
-}
-
-
-/// Add vertex to buffers
-static void addVertex(OglCanvas *c, double x, double y, 
-        double r, double g, double b, double a,
-        double u, double v)
-{
-    reserveSpace(c, 1);
-
-    int i = c->numVertices * 2;
-    c->vertexBuffer[i] = x;
-    c->vertexBuffer[i + 1] = y;
-
-    c->texBuffer[i] = u;
-    c->texBuffer[i + 1] = v;
-
-    i = c->numVertices * 4;
-    c->colorBuffer[i] = r;
-    c->colorBuffer[i + 1] = g;
-    c->colorBuffer[i + 2] = b;
-    c->colorBuffer[i + 3] = a;
- 
-    c->numVertices++;
-}
-
-/// draw vertices accumulated in buffers
-static void dumpBuffers(OglCanvas *c)
-{
-    if (c->numVertices) {
-        glDrawArrays(c->currentMode, 0, c->numVertices);
-        c->numVertices = 0;
-        c->batches++;
-    }
-}
-
 
 
 /// flush drawed graphics to screen
 static void drawEnd(struct XaGraphicsCallbacks *canvas)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
+/*    OglCanvas *c = (OglCanvas*)canvas;
     assert(canvas);
 
-    dumpBuffers(c);
-
-    glPopAttrib();
-    glPopClientAttrib();
-/*    printf("textures: %i (%i Kb) triangles: %i lines: %i  batches: %i\n", 
-            c->textures, c->texturesSize / 1024, c->triangles, c->lines,
-            c->batches);*/
-}
-
-
-// stop texturing
-static void disableTexture(OglCanvas *c)
-{
-    if (c->currentTexture) {
-        dumpBuffers(c);
-        glDisable(GL_TEXTURE_2D);
-        c->currentTexture = 0;
-    }
-}
-
-
-// start texturing
-static void setTexture(OglCanvas *c, int texId)
-{
-    if (c->currentTexture != texId) {
-        dumpBuffers(c);
-        if (! c->currentTexture)
-            glEnable(GL_TEXTURE_2D);
-        if (c->binderCallback)
-            c->binderCallback(texId);
-        else
-            glBindTexture(GL_TEXTURE_2D, texId);
-        c->currentTexture = texId;
-    }
-}
-
-
-// switch primitives to draw
-static void setMode(OglCanvas *c, int mode)
-{
-    if (c->currentMode != mode) {
-        dumpBuffers(c);
-        c->currentMode = mode;
-    }
+    printf("textures: %i %i triangles: %i lines: %i\n", 
+            c->textures, c->texturesSize / 1024, c->triangles, c->lines);*/
 }
 
 
@@ -217,7 +68,10 @@ static int loadTexture(struct XaGraphicsCallbacks *canvas,
     texId = id;
 
     // because of SOIL issue
-    setTexture(c, id);
+    if (c->binderCallback)
+        c->binderCallback(id);
+    else
+        glBindTexture(GL_TEXTURE_2D, id);
 
     if (width) {
         GLint w;
@@ -231,9 +85,7 @@ static int loadTexture(struct XaGraphicsCallbacks *canvas,
     }
 
     c->textures++;
-
-    if (width && height)
-        c->texturesSize += (*width) * (*height);
+    c->texturesSize += (*width) * (*height);
 
     return texId;
 }
@@ -256,11 +108,15 @@ static void drawLine(struct XaGraphicsCallbacks *canvas, double x1,
 
     c->lines++;
 
-    disableTexture(c);
-    setMode(c, GL_LINES);
+    glDisable(GL_TEXTURE_2D);
 
-    addVertex(c, x1, y1,  r, g, b, a, 0, 0);
-    addVertex(c, x2, y2,  r, g, b, a, 0, 0);
+    glColor4f(r, g, b, a);
+    glBegin(GL_LINES);
+      glVertex2f(x1, y1);
+      glVertex2f(x2, y2);
+    glEnd();
+    
+    glEnable(GL_TEXTURE_2D);
 }
 
 
@@ -275,12 +131,15 @@ static void drawTriangle(struct XaGraphicsCallbacks *canvas,
 
     c->triangles++;
 
-    disableTexture(c);
-    setMode(c, GL_TRIANGLES);
+    glDisable(GL_TEXTURE_2D);
 
-    addVertex(c, x1, y1,  r1, g1, b1, a1, 0, 0);
-    addVertex(c, x2, y2,  r2, g2, b2, a2, 0, 0);
-    addVertex(c, x3, y3,  r3, g3, b3, a3, 0, 0);
+    glBegin(GL_TRIANGLES);
+        glColor4d(r1, g1, b1, a1); glVertex2d(x1, y1);
+        glColor4d(r2, g2, b2, a2); glVertex2d(x2, y2);
+        glColor4d(r3, g3, b3, a3); glVertex2d(x3, y3);
+    glEnd();
+    
+    glEnable(GL_TEXTURE_2D);
 }
 
 
@@ -297,12 +156,16 @@ static void drawTexturedTriangle(struct XaGraphicsCallbacks *canvas,
     
     c->triangles++;
 
-    setTexture(c, textureId);
-    setMode(c, GL_TRIANGLES);
+    if (c->binderCallback)
+        c->binderCallback(textureId);
+    else
+        glBindTexture(GL_TEXTURE_2D, textureId);
 
-    addVertex(c,  x1, y1,  r1, g1, b1, a1,  u1, v1);
-    addVertex(c,  x2, y2,  r2, g2, b2, a2,  u2, v2);
-    addVertex(c,  x3, y3,  r3, g3, b3, a3,  u3, v3);
+    glBegin(GL_TRIANGLES);
+        glTexCoord2d(u1, v1); glColor4d(r1, g1, b1, a1); glVertex2d(x1, y1);
+        glTexCoord2d(u2, v2); glColor4d(r2, g2, b2, a2); glVertex2d(x2, y2);
+        glTexCoord2d(u3, v3); glColor4d(r3, g3, b3, a3); glVertex2d(x3, y3);
+    glEnd();
 }
 
 
@@ -310,36 +173,24 @@ static void drawTexturedTriangle(struct XaGraphicsCallbacks *canvas,
 static void setClipArea(struct XaGraphicsCallbacks *canvas, 
         double x1, double y1, double x2, double y2)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
-    assert(canvas);
-    dumpBuffers(c);
 }
 
 
 // disable clipping.
 static void resetClipArea(struct XaGraphicsCallbacks *canvas)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
-    assert(canvas);
-    dumpBuffers(c);
 }
 
 
 // push affine translation state
 static void pushTransform(struct XaGraphicsCallbacks *canvas)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
-    assert(canvas);
-    dumpBuffers(c);
     glPushMatrix();
 }
 
 // pop affine transform state
 static void popTransform(struct XaGraphicsCallbacks *canvas)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
-    assert(canvas);
-    dumpBuffers(c);
     glPopMatrix();
 }
 
@@ -348,9 +199,6 @@ static void popTransform(struct XaGraphicsCallbacks *canvas)
 static void translateTransform(struct XaGraphicsCallbacks *canvas, 
         double x, double y)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
-    assert(canvas);
-    dumpBuffers(c);
     glTranslated(x, y, 0);
 }
 
@@ -359,9 +207,6 @@ static void translateTransform(struct XaGraphicsCallbacks *canvas,
 static void scaleTransform(struct XaGraphicsCallbacks *canvas, 
         double x, double y)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
-    assert(canvas);
-    dumpBuffers(c);
     glScaled(x, y, 1.0f);
 }
 
@@ -369,9 +214,6 @@ static void scaleTransform(struct XaGraphicsCallbacks *canvas,
 static void rotateTransform(struct XaGraphicsCallbacks *canvas, 
         double angle)
 {
-    OglCanvas *c = (OglCanvas*)canvas;
-    assert(canvas);
-    dumpBuffers(c);
     glRotated(angle, 0, 0, -1.0);
 }
 
@@ -379,31 +221,26 @@ static void rotateTransform(struct XaGraphicsCallbacks *canvas,
 // initializa canvas structure
 struct XaGraphicsCallbacks* xagl_init_graphics()
 {
-    OglCanvas *c = new OglCanvas;
+    OglCanvas *canvas = new OglCanvas;
 
-    memset(c, 0, sizeof(OglCanvas));
+    memset(canvas, 0, sizeof(OglCanvas));
 
-    c->callbacks.draw_begin = drawBegin;
-    c->callbacks.draw_end = drawEnd;
-    c->callbacks.load_texture = loadTexture;
-    c->callbacks.free_texture = freeTexture;
-    c->callbacks.draw_line = drawLine;
-    c->callbacks.draw_triangle = drawTriangle;
-    c->callbacks.draw_textured_triangle = drawTexturedTriangle;
-    c->callbacks.set_clip_area = setClipArea;
-    c->callbacks.reset_clip_area = resetClipArea;
-    c->callbacks.push_transform = pushTransform;
-    c->callbacks.pop_transform = popTransform;
-    c->callbacks.translate_transform = translateTransform;
-    c->callbacks.scale_transform = scaleTransform;
-    c->callbacks.rotate_transform = rotateTransform;
-    
-    c->maxVertices = 1024;
-    c->vertexBuffer = (float*)malloc(sizeof(float) * 2 * c->maxVertices);
-    c->texBuffer = (float*)malloc(sizeof(float) * 2 * c->maxVertices);
-    c->colorBuffer = (float*)malloc(sizeof(float) * 4 * c->maxVertices);
+    canvas->callbacks.draw_begin = drawBegin;
+    canvas->callbacks.draw_end = drawEnd;
+    canvas->callbacks.load_texture = loadTexture;
+    canvas->callbacks.free_texture = freeTexture;
+    canvas->callbacks.draw_line = drawLine;
+    canvas->callbacks.draw_triangle = drawTriangle;
+    canvas->callbacks.draw_textured_triangle = drawTexturedTriangle;
+    canvas->callbacks.set_clip_area = setClipArea;
+    canvas->callbacks.reset_clip_area = resetClipArea;
+    canvas->callbacks.push_transform = pushTransform;
+    canvas->callbacks.pop_transform = popTransform;
+    canvas->callbacks.translate_transform = translateTransform;
+    canvas->callbacks.scale_transform = scaleTransform;
+    canvas->callbacks.rotate_transform = rotateTransform;
 
-    return (struct XaGraphicsCallbacks*)c;
+    return (struct XaGraphicsCallbacks*)canvas;
 }
 
 
@@ -411,12 +248,8 @@ struct XaGraphicsCallbacks* xagl_init_graphics()
 void xagl_done_graphics(struct XaGraphicsCallbacks *canvas)
 {
     OglCanvas *c = (OglCanvas*)canvas;
-    if (c) {
-        free(c->vertexBuffer);
-        free(c->texBuffer);
-        free(c->colorBuffer);
+    if (c)
         delete c;
-    }
 }
 
 void xagl_set_texture2d_binder_callback(struct XaGraphicsCallbacks *canvas, 
